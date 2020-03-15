ea09bd208cb38139e1fcd2a5c1c8a73b45696b9b
liximing <liximing127@gmail.com>
2020-03-15 17:30:52 +0900

    init

---
+++ src/main/java/com/johnson/mypages/MypagesApplication.java
@@ -0,0 +1,13 @@
+package com.johnson.mypages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/johnson/mypages/MypagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnson.mypages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
